75c13eaa9968123243f5550a0d05397e527289b9
waseem <waseemjaved755@gmail.com>
2022-09-01 03:06:27 +0500

    first commit

---
+++ test/inbox.test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3 (ganache.provider());
+ let accounts;
+ let inbox;
+ const {interface , bytecode}= require('../compile');
+
+beforeEach(async ()=>
+{
+  accounts = await web3.eth.getAccounts()
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({data : bytecode, arguments : ['Hi there']})
+  .send({from : accounts[0], gas:'1000000'})
+});
+
+describe('inbox' , ()=>
+{
+   it('deploys a contract', ()=> {
+
+    console.log(inbox);
+   });
+});
+/*describe('Car',()=>
+{
+   it('can park' , ()=>
+   {
+     const car=new Car();
+     assert.equal(car.park(),'stopped');
+   } );
+} );*/
