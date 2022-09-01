857c3c06b5dba0d107f8066a8c6c3c3b00420a69
waseem <waseemjaved755@gmail.com>
2022-09-01 06:41:16 +0500

    initial

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -18,7 +18,13 @@ describe('inbox' , ()=>
 {
    it('deploys a contract', ()=> {
 
-    console.log(inbox);
+   assert.ok(inbox.options.address);
+   });
+
+   it('has a default message', async ()=>
+   {
+     const message= await inbox.methods.message().call();
+     assert.equal(message, 'Hi there');
    });
 });
 /*describe('Car',()=>
